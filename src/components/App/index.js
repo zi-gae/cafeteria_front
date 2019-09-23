a064a7e66a4ebc1872b9cb035e62e4128f92b703
devGW <doscmDev@gmail.com>
2019-09-24 02:50:58 +0900

    feat: create footer

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 import styles from "./styles.scss";
+import Footer from "components/Footer";
 class App extends Component {
   render() {
-    return <div className={styles.App}></div>;
+    return (
+      <div className={styles.App}>
+        <Footer></Footer>
+      </div>
+    );
   }
 }
 
---
+++ src/components/Footer/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./styles.scss";
+
+const Footer = (props, context) => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.colum}>
+        <nav className={styles.nav}>
+          <ul className={styles.list}>
+            <li className={styles.listItem}>
+              <span className={styles.copyRight}>© 동명대학식이</span>
+            </li>
+            <li className={styles.listItem}>공지사항</li>
+            <li className={styles.listItem}>문의하기</li>
+            <li className={styles.listItem}>커뮤니티이용규칙</li>
+            <li className={styles.listItem}>개인정보처리방침</li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
