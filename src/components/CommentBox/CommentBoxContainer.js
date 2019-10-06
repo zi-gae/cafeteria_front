31bd1c391d50581c493de1151ab4d4e57e67fc61
devGW <doscmDev@gmail.com>
2019-10-07 01:33:02 +0900

    feat: commentBoxContainer init
    서버 작업 해야함

--- src/components/CommentBox/CommentBoxContainer.js
+++ src/components/CommentBox/CommentBoxContainer.js
@@ -2,8 +2,36 @@ import React, { Component } from "react";
 import CommentBoxPresenter from "./CommentBoxPresenter";
 
 class CommentBoxContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      comment: ""
+    };
+  }
+  _handleInputChange = e => {
+    const {
+      target: { value }
+    } = e;
+    this.setState({
+      comment: value
+    });
+  };
+  _handleKeyPress = e => {
+    const { key } = e;
+    if (key === "Enter") {
+      e.preventDeault();
+    }
+  };
   render() {
-    return <CommentBoxPresenter></CommentBoxPresenter>;
+    const { comment } = this.state;
+    const { _handleInputChange, _handleKeyPress } = this;
+    return (
+      <CommentBoxPresenter
+        handleInputChange={_handleInputChange}
+        handleKeyPress={_handleKeyPress}
+        comment={comment}
+      ></CommentBoxPresenter>
+    );
   }
 }
 
--- src/components/CommentBox/CommentBoxPresenter.js
+++ src/components/CommentBox/CommentBoxPresenter.js
@@ -2,15 +2,32 @@ import React from "react";
 import styles from "./styles.scss";
 import Textarea from "react-textarea-autosize";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
 const Container = styled.form``;
 
-const CommentBoxPresenter = props => {
+const CommentBoxPresenter = ({
+  comment,
+  handleInputChange,
+  handleKeyPress
+}) => {
   return (
     <Container className={styles.commentBox}>
-      <Textarea className={styles.input} placeholder="댓글 입력..." />
+      <Textarea
+        className={styles.input}
+        placeholder="댓글 입력..."
+        value={comment}
+        onChange={handleInputChange}
+        onKeyPress={handleKeyPress}
+      />
     </Container>
   );
+};
+
+CommentBoxPresenter.propTypes = {
+  comment: PropTypes.string.isRequired,
+  handleInputChange: PropTypes.func.isRequired,
+  handleKeyPress: PropTypes.func.isRequired
 };
 
 export default CommentBoxPresenter;
