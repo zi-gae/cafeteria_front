ac968c971fc9716ad5e9d8b2dfb29484cb30548c
devGW <doscmDev@gmail.com>
2019-10-07 23:26:24 +0900

    fix: replace 문구

--- src/components/TimeStamp/TimeStampContainer.js
+++ src/components/TimeStamp/TimeStampContainer.js
@@ -6,6 +6,9 @@ const TimeStampContainer = ({ time }) => {
   time = time.replace("minutes", "분");
   time = time.replace("a minute", "1분");
   time = time.replace("now", "지금");
+  time = time.replace("hours", "시간");
+  time = time.replace("hour", "시간");
+  time = time.replace("an", "한");
   return <TimeStampPresenter time={time}></TimeStampPresenter>;
 };
 
