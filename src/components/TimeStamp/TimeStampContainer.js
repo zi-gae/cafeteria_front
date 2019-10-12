efae2625d82a939e6a9816f05b0cb276b9fb7114
devGW <doscmDev@gmail.com>
2019-10-13 03:13:40 +0900

    refactor: props 값에 따른 마진값 변경

--- src/components/TimeStamp/TimeStampContainer.js
+++ src/components/TimeStamp/TimeStampContainer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import TimeStampPresenter from "./TimeStampPresenter";
 
-const TimeStampContainer = ({ time }) => {
+const TimeStampContainer = ({ title, time }) => {
   time = time.replace("ago", "전");
   time = time.replace("minutes", "분");
   time = time.replace("a minute", "1분");
@@ -11,7 +11,7 @@ const TimeStampContainer = ({ time }) => {
   time = time.replace("an", "한");
   time = time.replace("seconds", "초");
   time = time.replace("a seconds", "1초");
-  return <TimeStampPresenter time={time}></TimeStampPresenter>;
+  return <TimeStampPresenter title={title} time={time} />;
 };
 
 export default TimeStampContainer;
--- src/components/TimeStamp/TimeStampPresenter.js
+++ src/components/TimeStamp/TimeStampPresenter.js
@@ -3,12 +3,17 @@ import styles from "./styles.scss";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-const TimeStamp = styled.span``;
+const TimeStamp = styled.span`
+  color: #737373;
+  margin-top: ${props => (props.title ? "5px" : "15px")};
+  margin-bottom: 10px;
+  display: block;
+  font-size: 13px;
+`;
 
-const TimeStampPresenter = ({ time }) => (
-  <TimeStamp className={styles.time}>{time}</TimeStamp>
+const TimeStampPresenter = ({ title, time }) => (
+  <TimeStamp title={title}>{time}</TimeStamp>
 );
-
 TimeStampPresenter.propTypes = {
   time: PropTypes.string.isRequired
 };
